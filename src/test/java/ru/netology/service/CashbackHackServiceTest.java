1d5bac1ac2361ec30a5d200c82d02734222b04d0
Tom <gts-2010@mail.ru>
2023-04-21 13:44:10 +0500

    Initial commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -25,7 +25,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         int expected = 0;
 
-        assertEquals(expected, actual);
+        assertEquals( actual,expected);
     }
 
 }
